bb267b24af711da4d767f4c7dd0bebcadcb6327c
briskt <3172830+briskt@users.noreply.github.com>
2026-01-16 14:59:24 +0800

    copy from terraform-modules version 8.14.3
    Using terraform-modules version 8.14.3 as starting point for this module. The code is directly copied from the aws/ecs/cluster module.

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,27 @@
-output "output_name" {
-  value       = var.variable_name
-  description = "description of the output"
-  sensitive   = true
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.ecs_cluster.id
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.ecs_cluster.name
+}
+
+output "ecs_instance_role_id" {
+  value = aws_iam_role.ecsInstanceRole.id
+}
+
+output "ecs_instance_profile_id" {
+  value = aws_iam_instance_profile.ecsInstanceProfile.id
+}
+
+output "ecsServiceRole_arn" {
+  value = aws_iam_role.ecsServiceRole.arn
+}
+
+output "ecsInstanceRole_arn" {
+  value = aws_iam_role.ecsInstanceRole.arn
+}
+
+output "ami_id" {
+  value = data.aws_ami.ecs_ami.id
 }
